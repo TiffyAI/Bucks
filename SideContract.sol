e02ea32fc1b99ff7663f903b0319e94c9e0cd11f
TiffyAI <quentenmeyer@gmail.com>
2025-09-25 08:32:07 +0200

    Update SideContract.sol

--- SideContract.sol
+++ SideContract.sol
@@ -5,21 +5,47 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SideContract is Ownable {
     address public tiffy;
+    address public wbnb = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
+    address public router = 0x10ED43C718714eb63d5aA57B78B54704E256024E;
+    address public pool = 0x1305302e...; // Replace
 
     constructor(address _tiffy) Ownable(msg.sender) {
         tiffy = _tiffy;
     }
 
     function feedPool(uint256 amount) external {
-        // Transfer TIFFY to pair (0x1305302e...)
         (bool success,) = tiffy.call(
-            abi.encodeWithSignature("transfer(address,uint256)", 0x1305302e..., amount)
+            abi.encodeWithSignature("transfer(address,uint256)", pool, amount)
         );
         require(success, "TIFFY transfer failed");
     }
 
+    function addLiquidity(uint256 tiffyAmount, uint256 wbnbAmount) external {
+        (bool success1,) = tiffy.call(
+            abi.encodeWithSignature("approve(address,uint256)", router, tiffyAmount)
+        );
+        require(success1, "TIFFY approve failed");
+        (bool success2,) = wbnb.call(
+            abi.encodeWithSignature("approve(address,uint256)", router, wbnbAmount)
+        );
+        require(success2, "WBNB approve failed");
+        (bool success3,) = router.call(
+            abi.encodeWithSignature(
+                "addLiquidity(address,address,uint256,uint256,uint256,uint256,address,uint256)",
+                tiffy,
+                wbnb,
+                tiffyAmount,
+                wbnbAmount,
+                tiffyAmount * 95 / 100,
+                wbnbAmount * 95 / 100,
+                msg.sender,
+                block.timestamp + 300
+            )
+        );
+        require(success3, "Liquidity add failed");
+    }
+
     function setExempt(address[] memory wallets, bool exempt) external onlyOwner {
-        // Logic to set fee exemptions in TIFFY contract
         (bool success,) = tiffy.call(
             abi.encodeWithSignature("setFeeExempt(address[],bool)", wallets, exempt)
         );
