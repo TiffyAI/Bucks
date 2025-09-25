229f9bba32c3a95cc87e31240a460170787f0fa8
TiffyAI <quentenmeyer@gmail.com>
2025-09-25 04:33:46 +0200

    Create deploy.js

---
+++ deploy.js
@@ -0,0 +1,16 @@
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+  console.log("Deploying with:", deployer.address);
+
+  const SideContract = await hre.ethers.getContractFactory("SideContract");
+  const side = await SideContract.deploy("0xE488253D..."); // TIFFY address
+  await side.waitForDeployment();
+  console.log("SideContract deployed to:", await side.getAddress());
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
