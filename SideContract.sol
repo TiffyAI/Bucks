a29de2ea2f68031d4776b3c0c818654677ddaf96
TiffyAI <quentenmeyer@gmail.com>
2025-09-28 09:19:53 +0200

    Update SideContract.sol

--- SideContract.sol
+++ SideContract.sol
@@ -7,7 +7,7 @@ contract SideContract is Ownable {
     address public tiffy;
     address public wbnb = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
     address public router = 0x10ED43C718714eb63d5aA57B78B54704E256024E;
-    address public pool = 0x1305302e...; // Replace with your TIFFY/WBNB pair address
+    address public pool = 0x1305302e...; // Replace with TIFFY/WBNB pair address
 
     constructor(address _tiffy) Ownable(msg.sender) {
         tiffy = _tiffy;
@@ -50,5 +50,9 @@ contract SideContract is Ownable {
             abi.encodeWithSignature("setFeeExempt(address[],bool)", wallets, exempt)
         );
         require(success, "Exemption failed");
+    }
+
+    function withdrawBNB(uint256 amount) external onlyOwner {
+        payable(owner()).transfer(amount);
     }
 }
