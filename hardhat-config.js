3ec81ab66589b2ba55685d3622306b64a6323d53
TiffyAI <quentenmeyer@gmail.com>
2025-09-29 23:02:22 +0200

    Update hardhat-config.js

--- hardhat-config.js
+++ hardhat-config.js
@@ -1,12 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.20",
   networks: {
     bsc: {
       url: "https://bsc-dataseed.binance.org/",
-      accounts: [process.env.ADMIN_PRIVATE]
+      accounts: [process.env.ADMIN_PRIVATE] // Admin wallet private key (0x2a234...)
     }
+  },
+  etherscan: {
+    apiKey: process.env.BSCSCAN_API_KEY // Add BscScan API key
   }
 };
